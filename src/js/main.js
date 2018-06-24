5d556e874c7681406dab550c4c400f550d1a8e83
AlexXsWx <psy.suport@gmail.com>
2018-06-24 17:41:59 +0200

    added loading progress feedback

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@ requirejs.config({
     paths: {
         d3:           '../../lib/d3/3.5.6/d3.min',
         SmoothScroll: '../../lib/SmoothScroll/1.4.0/SmoothScroll'
-    }
+    },
+    onNodeCreated: createLoadingCounter()
 });
 
 // TODO: localize html
@@ -24,3 +25,48 @@ requirejs(
     }
 
 );
+
+//
+
+function createLoadingCounter() {
+
+    var domCache = {
+        loaded: document.getElementById('loading-loaded'),
+        amount: document.getElementById('loading-amount')
+    };
+
+    var loaded = 0;
+    var amount = 0;
+
+    return onNodeCreated;
+
+    function onNodeCreated(node, config, moduleName, url) {
+        onModuleAboutToBeLoaded(moduleName);
+        node.addEventListener('load',  function(event) { onModuleLoaded(moduleName); });
+        node.addEventListener('error', function(event) { onModuleFailedToLoad(moduleName); });
+    }
+
+    function onModuleAboutToBeLoaded(moduleName) {
+        amount++;
+        updateFeedback();
+    }
+
+    function onModuleLoaded(moduleName) {
+        loaded++;
+        updateFeedback();
+    }
+
+    function onModuleFailedToLoad(moduleName) {
+        console.error('module ' + moduleName + ' could not be loaded');
+    }
+
+    function updateFeedback() {
+        setTextContent(domCache.loaded, loaded);
+        setTextContent(domCache.amount, amount);
+    }
+
+    function setTextContent(element, text) {
+        element.innerHTML = '';
+        element.appendChild(document.createTextNode(text));
+    }
+}
